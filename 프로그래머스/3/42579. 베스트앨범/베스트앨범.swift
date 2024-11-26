4ab00653c28fc1928e7549d21016987124ebf30a
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2024-11-26 15:55:48 +0900

    [level 3] Title: 베스트앨범, Time: 0.36 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.swift
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.swift
@@ -1,5 +1,7 @@
 import Foundation
 
+typealias Genre = String
+
 struct GenreDetails {
     private(set) var songs: [Song] = []
     private(set) var totalPlays: Int = 0
@@ -8,15 +10,45 @@ struct GenreDetails {
         songs.append(song)
         totalPlays += song.played
     }
+    
+    func getTwoMaxValues() -> [Int] {
+        var first = Song()
+        var second = Song()
+        
+        for song in songs {
+            if song.played > first.played {
+                second = first
+                first = song
+            } else if song.played > second.played {
+                second = song
+            }
+        }
+        
+        if first.played == second.played && first.index! > second.index! {
+            return [second.index!, first.index!]
+        } else {
+            return [first.index!, second.index!]
+        }
+    }
 }
 
 struct Song {
     let played: Int
-    let index: Int
+    let index: Int?
+    
+    init() {
+        self.played = 0
+        self.index = nil
+    }
+    
+    init(played: Int, index: Int?) {
+        self.played = played
+        self.index = index
+    }
 }
 
 func solution(_ genres:[String], _ plays:[Int]) -> [Int] {
-    var hashMap: [String: GenreDetails] = [:]
+    var hashMap: [Genre: GenreDetails] = [:]
     var result: [Int] = []
     
     for index in 0..<genres.count {
@@ -30,40 +62,10 @@ func solution(_ genres:[String], _ plays:[Int]) -> [Int] {
     
     for genre in sortedByTotalPlays {
         
-        switch genre.value.songs.count {
-        case 1: result.append(genre.value.songs[0].index)
-        case 2:
-            let first = genre.value.songs[0]
-            let second = genre.value.songs[1]
-            
-            if first.played > second.played {
-                result.append(first.index)
-                result.append(second.index)
-            } else if first.played < second.played {
-                result.append(second.index)
-                result.append(first.index)
-            } else if first.index < second.index {
-                result.append(first.index)
-                result.append(second.index)
-            } else if first.index > second.index {
-                result.append(second.index)
-                result.append(first.index)
-            }
-        case 3...:
-            var inOrder = genre.value.songs.sorted { $0.played < $1.played }
-            
-            let last = inOrder.popLast()
-            let secondLast = inOrder.popLast()
-            
-            if last?.played == secondLast?.played && last!.index > secondLast!.index {
-                result.append((secondLast?.index)!)
-                result.append((last?.index)!)
-            } else {
-                result.append((last?.index)!)
-                result.append((secondLast?.index)!)
-            }
-
-        default: print("error")
+        if genre.value.songs.count == 1 {
+            result.append(genre.value.songs[0].index!)
+        } else if genre.value.songs.count >= 2 {
+            result.append(contentsOf: genre.value.getTwoMaxValues())
         }
     }
 
