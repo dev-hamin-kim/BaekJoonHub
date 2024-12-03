82edfe0ac459712d2d54c837d5fd2ec840027ca3
dev-hamin-kim <129831340+dev-hamin-kim@users.noreply.github.com>
2024-12-03 11:34:31 +0900

    [level 2] Title: 올바른 괄호, Time: 10.97 ms, Memory: 16.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.swift
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.swift
@@ -1,27 +1,24 @@
 import Foundation
-
 func solution(_ s:String) -> Bool {
     
-    // 첫 문자가 ")"일 경우, 무조건 닫히지 않은 괄호이니, false를 리턴
-    guard s.first == "(" || s.last == ")" else { return false }
+    // 첫 문자가 ")" 경우 false를 리턴
+    guard s.first == "(" else { return false }
     
     var answer = false
     var stack: [Character] = []
-
     for char in s {
         
-        
+        // 문자열 중
         switch char {
         case "(": stack.append(char)
-        case ")": if stack.popLast() == nil { return false }
-        default: // 문자열 중 괄호 외의 문자를 발견 시 false를 리턴하도록 하는
-            return false
+        case ")": stack.popLast()
+        default:
+            answer = false
+            break
         }
     }
-
     if stack.isEmpty {
         answer = true
     }
-
     return answer
 }
